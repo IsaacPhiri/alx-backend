2f2eee899ffb8c6f78b016ef0b2cf81de4014a7b
IsaacPhiri <isaacphiri315@gmail.com>
2023-04-18 12:41:33 +0000

    Added task one and 2 readme files

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+basic_cache module
+"""
+
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    basicCache class
+    """
+
+    def put(self, key, item):
+        """
+        Adds items to the cache
+        """
+
+        if key in None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Retrieves itmes from the cache
+        """
+
+        if key is None or key not in self.cache_data:
+            return None
+        return self.chache_data[key]
