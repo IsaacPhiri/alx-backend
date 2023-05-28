f70ec6ce8339645ae109744653b773fcda85eb3e
IsaacPhiri <isaacphiri315@gmail.com>
2023-05-28 15:37:01 +0000

    Create redisClient module

--- utils/redis.js
+++ utils/redis.js
@@ -20,11 +20,29 @@ class RedisClient {
 			return false;
 		};
 		async function get(key){
-			const value = await this.client.get(key, redis.print);
-			return value;
+			try {
+				const value = await this.client.get(key, redis.print);
+				return value;
+			} catch (error) {
+				throw new Error('Could not get value.');
+			}
 		};
-		async function set(key, value) {
-			await this.client.set(key, value, redis.print);
-		}
+		async function set(key, value, time) {
+			setTimeout(() => {
+			try {
+				await this.client.set(key, value, redis.print);
+			} catch (error) {
+				throw new Error('Could not set key:value for client.');
+			}, time)};
+		};
+		async function del(key) {
+			try {
+				await this.client.del(key);
+			} catch (error) {
+				throw new Error('Could not delete key.');
+			}
+		};
         }
 }
+
+const redisClient = new RedisClient;
