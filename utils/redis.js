60f97afe3b9e033ae8a97446faa515e28dbe5393
IsaacPhiri <isaacphiri315@gmail.com>
2023-05-28 14:49:57 +0000

    Working on the async functions -get and set

--- utils/redis.js
+++ utils/redis.js
@@ -7,11 +7,24 @@ class RedisClient {
                         port: port,
                 });
 
-                this.client.on('client', () => {
+                this.client.on('connect', () => {
                         console.log('Redis client connected.');
                 });
                 this.client.on('error', () => {
                         console.log('Conncetion to server failed!', error);
                 });
+		isAlive() {
+			this.client.on('connect', () => {
+				return true;
+			});
+			return false;
+		};
+		async function get(key){
+			const value = await this.client.get(key, redis.print);
+			return value;
+		};
+		async function set(key, value) {
+			await this.client.set(key, value, redis.print);
+		}
         }
 }
